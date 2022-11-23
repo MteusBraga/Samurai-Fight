c6be68195ad2dd937a01da03b96c2f45decd0040
Mateus Braga <mateus.braga@academico.ifpb.edu.br>
2022-11-23 05:19:10 -0300

    background srpite & shop sprite animation

--- index.js
+++ index.js
@@ -6,62 +6,26 @@ canvas.height = 576//proporcoes da tela
 const gravity = 0.7
 //retangulo preto do tamanho da tela
 c.fillRect(0, 0,canvas.width ,canvas.height)
-class Sprite{ // classe de sprite
-    constructor({position, velocity, color = 'red', offset}){
-        this.position = position
-        this.velocity = velocity
-        this.width = 50
-        this.height = 150
-        this.lastKey
-        this.attackBox = {
-            position: {
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset: offset,
-            width: 100,
-            height: 50,
-        }
-        this.color = color
-        this.isAttacking
-        this.health = 100
-    }
 
-    draw(){
-        c.fillStyle = this.color //funcao desenhar um sprite, preenche de vermelho e cria um retangulo
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)//retangulo (propocoes)
+const background = new Sprite({
+    position:{
+        x: 0,
+        y:0
+    },
+    imageSrc: './img/background.png'
+})
 
-        //attack box
-        if(this.isAttacking){
-            c.fillStyle = 'green'
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-        }
-       
-    }
+const shop = new Sprite({
+    position:{
+        x: 600,
+        y: 128
+    },
+    imageSrc: './img/shop.png',
+    scale: 2.75,
+    frameMax: 6
+})
 
-    update(){
-        this.draw()
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y 
-        this.position.x += this.velocity.x 
-
-        this.position.y += this.velocity.y
-
-        if(this.position.y + this.height + this.velocity.y>=canvas.height) { //detectando o chao
-            this.velocity.y = 0
-        }else{
-            this.velocity.y += gravity
-        }
-    }
-    attack(){
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100)
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position: {//criando o player
     x: 0,
     y: 0
@@ -78,7 +42,7 @@ const player = new Sprite({
 
 
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position: {//criando o enemy
     x: 400,
     y: 100
@@ -113,37 +77,7 @@ const keys={
     }
 }
 
-function rectangularCollision({rectangle1, rectangle2}){
-    return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && 
-            rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-            rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
-            rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height)
-}
 
-function determineWinner({player, enemy, timerId}){
-    clearTimeout(timerId)
-    document.querySelector('#displayText').style.display = 'flex'
-    if(player.health === enemy.health){
-        document.querySelector('#displayText').innerHTML ='Tie'
-    } else if(player.health > enemy.health){
-        document.querySelector('#displayText').innerHTML ='Player 1 wins'
-    }else if(player.health < enemy.health){
-        document.querySelector('#displayText').innerHTML ='Player 2 wins'
-    }
-}
-
-let timer = 60
-let timerId 
-function decreaseTimer(){
-    if(timer > 0) {
-        timerId=setTimeout(decreaseTimer, 1000)
-        timer--
-        document.querySelector('#timer').innerHTML = timer
-    }
-    if(timer === 0){
-        determineWinner({player, enemy, timerId})
-    }
-}
 
 decreaseTimer()
 
@@ -151,8 +85,11 @@ function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0,0,canvas.width,canvas.height)
+    background.update()
+    shop.update()
     player.update()
     enemy.update()
+    
 
     player.velocity.x = 0
     enemy.velocity.x = 0
